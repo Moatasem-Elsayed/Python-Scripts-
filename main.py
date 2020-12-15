306240ab867b732e11fdf37821e40abd18c0ea3d
Moatasem El Sayed <66727825+Moatasem-Elsayed@users.noreply.github.com>
2020-12-15 22:25:16 +0200

    post on multiple  group on facebook

---
+++ main.py
@@ -0,0 +1,72 @@
+# import pyautogui
+# import time
+#
+# groups = [ 'arduino.junction.297', 'Embedded.moatasem.elsayed', 'Arduino.from.one.to.done.junction.220',
+#            '1704995113130213', 'arduino.junction.187', '117581965590777', '705088266368688',
+#            'arduino.junction200', 'Embedded.system.diploma.junction218',
+#           '427954221497056', '749791112090508', '261626251224892', '1377913009030120',
+#           '1794458307340093', '594858277848685', '331021771111609', 'arduino.junction.212']
+#
+# time.sleep(5)
+#
+#
+# def changelag():
+#     pyautogui.keyDown('shift')
+#     pyautogui.keyDown('alt')
+#     pyautogui.keyUp('alt')
+#     pyautogui.keyUp('shift')
+#
+#
+# pyautogui.keyDown('ctrl')
+# pyautogui.keyDown('t')
+# pyautogui.keyUp('t')
+# pyautogui.keyUp('ctrl')
+#
+# for i in range(len(groups)):
+#     link = 'https://facebook.com/groups/' + groups[i]
+#     pyautogui.typewrite(link)
+#     pyautogui.typewrite('\n')
+#
+#     pyautogui.moveTo(550, 400)  # Move the mouse to the x, y coordinates 100, 150.
+#     time.sleep(5)
+#     pyautogui.doubleClick()  # Double click the mouse at the
+#     print("Writing post\n")
+#     time.sleep(2)
+#     pyautogui.typewrite("shorturl.at/cBI69")
+#     pyautogui.typewrite("\n")
+#     time.sleep(5)
+#     changelag()
+#     time.sleep(2)
+#     pyautogui.typewrite(";,vs ;hlg hkahx hggi lk h,g gd] gp] hgiot ,oghwi ofvi 5 skdk ")
+#     changelag()
+#     pyautogui.moveTo(600, 620)  # Move the mouse to the x, y coordinates 100, 150.
+#     time.sleep(5)
+#     pyautogui.doubleClick()  # Double click the mouse at the
+#
+#     # # pyautogui.typewrite('p')
+#     # # time.sleep(2)
+#     # for j in range(1, 45):
+#     #     pyautogui.keyDown('tab')
+#     #     pyautogui.keyUp('tab')
+#     # pyautogui.keyDown('enter')
+#     # pyautogui.keyUp('enter')
+#     # j=0
+#     # for j in range(1, 13):
+#     #     pyautogui.keyDown('tab')
+#     #     pyautogui.keyUp('tab')
+#     #
+#     # pyautogui.keyDown('enter')
+#     # pyautogui.keyUp('enter')
+#
+#     pyautogui.keyDown('Esc')
+#     pyautogui.keyup('Esc')
+#
+#     pyautogui.keyDown('ctrl')
+#     pyautogui.keyDown('enter')
+#     pyautogui.keyUp('enter')
+#     pyautogui.keyUp('ctrl')
+#
+#     time.sleep(3)
+#
+#     pyautogui.write(['f6'])
+#     time.sleep(1)
