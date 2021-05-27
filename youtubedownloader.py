76904793202bd565cc790cd36c0b8cc2df3d2176
Moatasem El Sayed <66727825+Moatasem-Elsayed@users.noreply.github.com>
2021-05-27 10:51:11 +0200

    Add files via upload

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -1,23 +1,37 @@
 from pytube import YouTube ,Playlist
 from tkinter import filedialog  
 import os 
-
-select=int(input("1- PlayList \n 2- Single Video"))
+import time
+select=int(input("1- PlayList \n2- Single Video\n"))
 link=input("Please enter your url:")
 print("please choose the downloading path :")
 pathsave= filedialog.askdirectory() 
-
+cnt=0
+    
 def finish():
     print("dowanload is done")
     os.startfile(pathsave)
+    cnt=0
+    
+def progress():
+    global cnt 
+    cnt=cnt+1
+    print("Loading..",end=" ")
+    print(cnt)
+       
 
 if select == 2:   
     video=YouTube(link)
     video.streams.get_highest_resolution().download(output_path=pathsave)
+    
 elif select == 1:
     playlist=Playlist(link)
     for video in playlist.videos:
          video.streams.get_highest_resolution().download(output_path=pathsave)
+         video.register_on_progress_callback(progress())
+         
 else :
     exit()
+
+
 video.register_on_complete_callback(finish())
